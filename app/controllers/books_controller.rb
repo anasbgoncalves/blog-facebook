a0f3b44f8cef0218ae711391214357db84e92639
AnnieLee <ana.sbgoncalves@gmail.com>
2017-05-24 18:37:55 +0100

    Movies and books list

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Facebook.get_object(current_user, '/me/books')
+    @books = Facebook.get_object(current_user, '/me/books?fields=name,picture')
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = Facebook.get_object(current_user, { id: '/me/movies'})
+    @movies = Facebook.get_object(current_user, '/me/movies?fields=name,picture')
   end
 end
